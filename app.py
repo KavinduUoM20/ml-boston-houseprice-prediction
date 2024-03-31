88f64c47aaa32b4f35243f8be8b63e92a26f9c07
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2024-03-31 07:26:24 +0530

    Completed Frontend

--- app.py
+++ app.py
@@ -21,8 +21,28 @@ def predict_api():
     data = request.json['data']
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
+    print(output)
     return jsonify(output[0])
 
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    try:
+        if request.method == 'POST' and request.form:
+            data = [float(x) for x in request.form.values()]
+            
+            if data:
+                final_input = scaler.transform(np.array(data).reshape(1, -1))
+                print(final_input)
+                output = regmodel.predict(final_input)[0]
+                return render_template('home.html', predicted_val=output)
+            else:
+                return "Error: No data submitted."
+        else:
+            return "Error: No form data received."
+    except Exception as e:
+        # Handle exceptions gracefully
+        return f"An error occurred: {str(e)}", 500
 
 if __name__== "__main__":
     app.run(debug=True)
